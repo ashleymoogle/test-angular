5a314340573e96dbb5c3fe56021095000f67adb6
Romain Bonhomme <romain.bonhomme89@gmail.com>
2017-01-17 17:18:24 +0100

    service worker fixed and cleaned a little

--- angular/sw.js
+++ angular/sw.js
@@ -1,72 +1,74 @@
 console.log('WORKER:: running.');
 
-const version = "15::"
+const version = "v1::"
+const staticCacheName = `${version}static-resources`;
 
-const offlineFundamentals = [
+const offlineStuff = [
     '/dist/app.bundle.css',
     '/dist/app.bundle.js'
 ]
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
     console.log('in install')
     // pre cache a load of stuff:
     event.waitUntil(
         caches
-            .open(version + 'fundamentals')
-            .then(function(cache) {
+            .open(staticCacheName)
+            .then((cache) => {
                 cache.add('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.1/angular.min.js')
-                return cache.addAll(offlineFundamentals);
+                return cache.addAll(offlineStuff);
 
             })
-            .then(function() {
+            .then(() => {
                 console.log('WORKER:: install completed');
             })
     )
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate',  (event) => {
     event.waitUntil(
         caches
             .keys()
-            .then(function (keys) {
+            .then( (keys) => {
                 // We return a promise that settles when all outdated caches are deleted.
                 return Promise.all(
                     keys
-                        .filter(function (key) {
+                        .filter( (key) => {
                             // Filter by keys that don't start with the latest version prefix.
                             return !key.startsWith(version);
                         })
-                        .map(function (key) {
+                        .map( (key) => {
                             return caches.delete(key);
                         })
                 );
             })
-            .then(function() {
+            .then(() => {
                 console.log('WORKER:: activate completed.');
             })
     )
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         // Try the cache
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then((response) => {
             // Fall back to network
             // if (!response) throw Error("No data");
             //console.log(response ? response : 'no cache' + event.request.url)
             return response || fetch(event.request);
-        }).catch(function() {
-            /*
-            console.log('no cache', event.request.url)
-            return caches.open(version + 'fundamentals').then(function(cache) {
-                return fetch(event.request).then(function(response) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-            })
-            */
+        }).catch(() => {
+            /*console.log('no cache', event.request.url)
+             return caches.open(version + 'fundamentals').then((cache) => {
+             return fetch(event.request).then((response) => {
+             cache.put(event.request, response.clone());
+             return response;
+             });
+             })*/
         })
     );
 });
+
+
+
 
 
